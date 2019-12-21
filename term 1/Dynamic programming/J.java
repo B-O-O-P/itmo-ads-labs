5ca783864aa803725bb298efb7bc150b58a8c359
Chizhikov Dmitriy <dimkanut@gmail.com>
2019-12-21 13:51:36 +0300

    Update and rename I.java to J.java

--- term 1/Dynamic programming/J.java
+++ term 1/Dynamic programming/J.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class I {
+public class J {
     static int infinity = 1000000000;
     static int dp[][];
     static int a[][];
